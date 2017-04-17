49746a52343d8d96527f823d6f0ce66464d0ae2f
illuminare <lonely.sandman@gmail.com>
2017-04-17 16:14:57 +0300

    Mouse giggler

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbltimer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labeltimeleft = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "(De)Activate timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Timer is ";
+            // 
+            // lbltimer
+            // 
+            this.lbltimer.AutoSize = true;
+            this.lbltimer.Location = new System.Drawing.Point(56, 13);
+            this.lbltimer.Name = "lbltimer";
+            this.lbltimer.Size = new System.Drawing.Size(44, 13);
+            this.lbltimer.TabIndex = 2;
+            this.lbltimer.Text = "inactive";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Time left:";
+            // 
+            // labeltimeleft
+            // 
+            this.labeltimeleft.AutoSize = true;
+            this.labeltimeleft.Location = new System.Drawing.Point(68, 45);
+            this.labeltimeleft.Name = "labeltimeleft";
+            this.labeltimeleft.Size = new System.Drawing.Size(49, 13);
+            this.labeltimeleft.TabIndex = 4;
+            this.labeltimeleft.Text = "00:09:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 72);
+            this.Controls.Add(this.labeltimeleft);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbltimer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mouse Jiggler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbltimer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labeltimeleft;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private Boolean timerstate = false;
+        private int timeticker = 0;
+        private int timetotal = 540;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info; //Shows the info icon so the user doesn't thing there is an error.
+            this.notifyIcon.BalloonTipText = "[Jiggling mouse]";
+            this.notifyIcon.BalloonTipTitle = "[Yeah, jiggling mouse]";
+            this.notifyIcon.Text = "[Yeah, jiggling mouse]";
+            this.Resize += ImportStatusForm_Resize;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timerstate;
+            timerstate = timer1.Enabled;
+            if (timerstate)
+            {
+                lbltimer.Text = "active";
+            }
+            else
+            {
+                lbltimer.Text = "not active";
+                timeticker = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeticker++;
+            var timeleft = timetotal - timeticker;
+            if (timeleft <= 0)
+            {
+                DoJiggle();
+                timeticker = 0;
+            }
+            TimeSpan span = new TimeSpan(0,0,timeleft);
+            labeltimeleft.Text = span.Hours.ToString() + ":" + span.Minutes.ToString() + ":" + span.Seconds.ToString();
+        }
+
+        private void DoJiggle()
+        {
+            Jiggler.Jiggle(0,1);
+            Application.DoEvents();
+            Jiggler.Jiggle(0, -1);
+            Application.DoEvents();
+        }
+
+        private void ImportStatusForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+                notifyIcon.ShowBalloonTip(3000);
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick_1(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon.Visible = false;
+        }
+    }
+}
